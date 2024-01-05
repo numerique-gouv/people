f4ff27636d9e6058616c65d21d3fe61111d47f03
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-01-05 15:39:17 +0100

    💄(app-desk) integrate cunningham design system
    Integrate cunningham design system into app desk.
    It comes with some boilerplate code that will have to be
    adapted to our needs when we will get the design.

---
+++ src/frontend/app/desk/src/cunningham/index.ts
@@ -0,0 +1,7 @@
+import { tokens } from './cunningham-tokens';
+
+const themeTokens = tokens.themes.default.theme;
+const colorsTokens = tokens.themes.default.theme.colors;
+const componentTokens = tokens.themes.default.components;
+
+export { tokens, themeTokens, componentTokens, colorsTokens };
