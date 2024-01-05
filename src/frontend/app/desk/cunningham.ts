2416f107019176a09bb6d532e35d34ddc85fc4eb
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-01-05 15:39:17 +0100

    💄(app-desk) integrate cunningham design system
    Integrate cunningham design system into app desk.
    It comes with some boilerplate code that will have to be
    adapted to our needs when we will get the design.

---
+++ src/frontend/app/desk/cunningham.ts
@@ -0,0 +1,179 @@
+const config = {
+  themes: {
+    default: {
+      theme: {
+        colors: {
+          'primary-100': '#EDF5FA',
+          'primary-150': '#E5EEFA',
+          'info-150': '#E5EEFA',
+        },
+        font: {
+          sizes: {
+            ml: '0.938rem',
+            xl: '1.50rem',
+            t: '0.6875rem',
+            s: '0.75rem',
+            h1: '2.2rem',
+            h2: '1.7rem',
+            h3: '1.37rem',
+            h4: '1.15rem',
+            h5: '1rem',
+            h6: '0.87rem',
+          },
+          weights: {
+            thin: 100,
+            extrabold: 800,
+            black: 900,
+          },
+        },
+        spacings: {
+          '0': '0',
+          none: '0',
+          auto: 'auto',
+          bx: '2.2rem',
+          full: '100%',
+        },
+        breakpoints: {
+          xxs: '320px',
+          xs: '480px',
+        },
+      },
+      components: {
+        datagrid: {
+          header: {
+            weight: 'var(--c--theme--font--weights--extrabold)',
+            size: 'var(--c--theme--font--sizes--ml)',
+          },
+          cell: {
+            color: 'var(--c--theme--colors--primary-500)',
+            size: 'var(--c--theme--font--sizes--ml)',
+          },
+        },
+        'forms-checkbox': {
+          'background-color': {
+            hover: '#055fd214',
+          },
+          color: 'var(--c--theme--colors--primary-500)',
+          'font-size': 'var(--c--theme--font--sizes--ml)',
+        },
+        'forms-datepicker': {
+          'border-color': 'var(--c--theme--colors--primary-500)',
+          'value-color': 'var(--c--theme--colors--primary-500)',
+          'border-radius': {
+            hover: 'var(--c--components--forms-datepicker--border-radius)',
+            focus: 'var(--c--components--forms-datepicker--border-radius)',
+          },
+        },
+        'forms-field': {
+          color: 'var(--c--theme--colors--primary-500)',
+          'value-color': 'var(--c--theme--colors--primary-500)',
+          width: 'auto',
+        },
+        'forms-input': {
+          'value-color': 'var(--c--theme--colors--primary-500)',
+          'border-color': 'var(--c--theme--colors--primary-500)',
+        },
+        'forms-labelledbox': {
+          'label-color': {
+            small: 'var(--c--theme--colors--primary-500)',
+            big: {
+              disabled: 'var(--c--theme--colors--greyscale-400)',
+            },
+          },
+        },
+        'forms-select': {
+          'border-color': 'var(--c--theme--colors--primary-500)',
+          'border-radius': {
+            hover: 'var(--c--components--forms-select--border-radius)',
+            focus: 'var(--c--components--forms-select--border-radius)',
+          },
+          'font-size': 'var(--c--theme--font--sizes--ml)',
+          'item-background-color': {
+            hover: 'var(--c--theme--colors--primary-100)',
+          },
+          'item-color': 'var(--c--theme--colors--primary-500)',
+          'value-color': 'var(--c--theme--colors--primary-500)',
+        },
+        'forms-switch': {
+          'accent-color': 'var(--c--theme--colors--primary-400)',
+        },
+        'forms-textarea': {
+          'border-color': 'var(--c--components--forms-textarea--border-color)',
+          'border-color-hover':
+            'var(--c--components--forms-textarea--border-color)',
+          'border-radius': {
+            hover: 'var(--c--components--forms-textarea--border-radius)',
+            focus: 'var(--c--components--forms-textarea--border-radius)',
+          },
+          color: 'var(--c--theme--colors--primary-500)',
+          disabled: {
+            'border-color-hover': 'var(--c--theme--colors--greyscale-200)',
+          },
+        },
+        button: {
+          'border-radius': {
+            active: 'var(--c--components--button--border-radius)',
+          },
+          'medium-height': 'auto',
+          'small-height': 'auto',
+          success: {
+            color: 'white',
+            'color-disabled': 'white',
+            'color-hover': 'white',
+            background: {
+              color: 'var(--c--theme--colors--success-600)',
+              'color-disabled': 'var(--c--theme--colors--greyscale-300)',
+              'color-hover': 'var(--c--theme--colors--success-800)',
+            },
+          },
+          danger: {
+            'color-hover': 'white',
+            background: {
+              color: 'var(--c--theme--colors--danger-400)',
+              'color-hover': 'var(--c--theme--colors--danger-500)',
+              'color-disabled': 'var(--c--theme--colors--danger-100)',
+            },
+          },
+          primary: {
+            color: 'var(--c--theme--colors--primary-text)',
+            'color-active': 'var(--c--theme--colors--primary-text)',
+            background: {
+              color: 'var(--c--theme--colors--primary-400)',
+              'color-active': 'var(--c--theme--colors--primary-500)',
+            },
+            border: {
+              'color-active': 'transparent',
+            },
+          },
+          secondary: {
+            color: 'var(--c--theme--colors--primary-500)',
+            'color-hover': 'var(--c--theme--colors--primary-text)',
+            background: {
+              color: 'white',
+              'color-hover': 'var(--c--theme--colors--primary-700)',
+            },
+            border: {
+              color: 'var(--c--theme--colors--primary-200)',
+            },
+          },
+          tertiary: {
+            color: 'var(--c--theme--colors--primary-500)',
+            'color-disabled': 'var(--c--theme--colors--greyscale-600)',
+            background: {
+              'color-hover': 'var(--c--theme--colors--primary-100)',
+              'color-disabled': 'var(--c--theme--colors--greyscale-200)',
+            },
+          },
+          disabled: {
+            color: 'white',
+            background: {
+              color: '#b3cef0',
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+export default config;
