05d9f6430dcc9839337de7d05884592b6c01d355
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-01-10 11:01:31 +0100

    🚨(app-desk) add css linter
    eslint is not enough for css, so we need a css linter too.
    This commit adds stylelint to the project, we configure it
    with prettier and add a check during the build process.

---
+++ src/frontend/app/desk/stylelint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  rules: {
+    'custom-property-pattern': null,
+    'selector-class-pattern': null,
+    'no-descending-specificity': null,
+  },
+};
