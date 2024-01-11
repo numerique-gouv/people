c87953156442fac933732364d00f058d78858c94
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-01-11 12:31:08 +0100

    ✅(project) install e2e playwright
    Install playwright, adapt the config file and add a scripts to
    run the tests.
    e2e testing will monitor all our frontend applications,
    so we install it in the frontend folder.
    It configures the base of our monorepo.

---
+++ src/frontend/apps/e2e/playwright.config.ts
@@ -0,0 +1,58 @@
+import { defineConfig, devices } from '@playwright/test';
+
+const PORT = process.env.PORT || 3200;
+
+const baseURL = `http://localhost:${PORT}`;
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  // Timeout per test
+  timeout: 30 * 1000,
+  testDir: './__tests__',
+  outputDir: './test-results',
+  
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [['html', { outputFolder: './report' }]],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    baseURL: baseURL,
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  webServer: {
+    command: 'cd ../.. && yarn dev:desk --port ' + PORT,
+    url: baseURL,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
