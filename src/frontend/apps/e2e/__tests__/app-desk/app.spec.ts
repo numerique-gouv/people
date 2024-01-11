5f280ae3fc122f780fd1da0d62cea8b5b82bdb07
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-01-11 12:32:45 +0100

    ✅(app-desk) e2e test app-desk
    Tests:
    - login to keycloak
    - create new teams
    - check teams are displayed

---
+++ src/frontend/apps/e2e/__tests__/app-desk/app.spec.ts
@@ -0,0 +1,28 @@
+import { expect, test } from '@playwright/test';
+
+import { keyCloakSignIn } from './common';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/');
+  await keyCloakSignIn(page);
+});
+
+test.describe('App', () => {
+  test('should display the homepage after keycloak login', async ({ page }) => {
+    await expect(page.locator('h2')).toContainText('Hello world!');
+  });
+
+  test('creates 2 teams and displayed them', async ({ page }) => {
+    await page.getByLabel('Team name').fill('My new team');
+    await page.click('button:has-text("Create Team")');
+    await page.getByLabel('Team name').fill('My second new team');
+    await page.click('button:has-text("Create Team")');
+
+    await expect(
+      page.locator('li').getByText('My new team').first(),
+    ).toBeVisible();
+    await expect(
+      page.locator('li').getByText('My second new team').first(),
+    ).toBeVisible();
+  });
+});
