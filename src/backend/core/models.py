cfc35ac23e22747ce0603769e05dd505bc7bd437
Samuel Paccoud - DINUM <samuel.paccoud@mail.numerique.gouv.fr>
2024-01-15 09:16:55 +0100

    ♻️(models) rename *_on fields to *_at
    This is only for display, nothing has changed in database.

--- src/backend/core/models.py
+++ src/backend/core/models.py
@@ -48,13 +48,13 @@ class BaseModel(models.Model):
         editable=False,
     )
     created_at = models.DateTimeField(
-        verbose_name=_("created on"),
+        verbose_name=_("created at"),
         help_text=_("date and time at which a record was created"),
         auto_now_add=True,
         editable=False,
     )
     updated_at = models.DateTimeField(
-        verbose_name=_("updated on"),
+        verbose_name=_("updated at"),
         help_text=_("date and time at which a record was last updated"),
         auto_now=True,
         editable=False,
