625e98515b1d5df20a726cc95f310eb87c84c8f2
Lebaud Antoine <lebaud.antoine131@gmail.com>
2024-01-26 10:42:00 +0100

    🔧(backend) activate container liveness probes
    Enabled Dockerflow Django app by activating liveness probes. The previously
    unavailable routes such as `__heartbeat__` and `__lbheartbeat__` are now
    accessible. New endpoints include:
    * GET /__version__
    * GET /__heartbeat__
    * GET /__lbheartbeat__

--- src/backend/people/settings.py
+++ src/backend/people/settings.py
@@ -175,6 +175,7 @@ class Base(Configuration):
         "django.middleware.csrf.CsrfViewMiddleware",
         "django.contrib.auth.middleware.AuthenticationMiddleware",
         "django.contrib.messages.middleware.MessageMiddleware",
+        "dockerflow.django.middleware.DockerflowMiddleware",
     ]
 
     AUTHENTICATION_BACKENDS = [
