958084c5bbb70d94a191e8bbf7a2cbf231d4f459
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-02-22 15:59:25 +0100

    🚚(app-desk) alias home with teams url path
    In order the keep the url path consistent and correctly
    structured, the homepage is aliased with the teams page.

--- src/frontend/apps/desk/src/pages/index.tsx
+++ src/frontend/apps/desk/src/pages/index.tsx
@@ -1,27 +1,12 @@
-import { Button } from '@openfun/cunningham-react';
 import type { ReactElement } from 'react';
-import { useTranslation } from 'react-i18next';
-import styled from 'styled-components';
 
-import { Box, StyledLink } from '@/components';
 import { NextPageWithLayout } from '@/types/next';
 
+import Teams from './teams/';
 import TeamLayout from './teams/TeamLayout';
 
-const StyledButton = styled(Button)`
-  width: fit-content;
-`;
-
 const Page: NextPageWithLayout = () => {
-  const { t } = useTranslation();
-
-  return (
-    <Box $align="center" $justify="center" $height="inherit">
-      <StyledLink href="/teams/create">
-        <StyledButton>{t('Create a new team')}</StyledButton>
-      </StyledLink>
-    </Box>
-  );
+  return <Teams />;
 };
 
 Page.getLayout = function getLayout(page: ReactElement) {
---
+++ src/frontend/apps/desk/src/pages/teams/index.tsx
@@ -0,0 +1,31 @@
+import { Button } from '@openfun/cunningham-react';
+import type { ReactElement } from 'react';
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+
+import { Box, StyledLink } from '@/components';
+import { NextPageWithLayout } from '@/types/next';
+
+import TeamLayout from './TeamLayout';
+
+const StyledButton = styled(Button)`
+  width: fit-content;
+`;
+
+const Page: NextPageWithLayout = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Box $align="center" $justify="center" $height="inherit">
+      <StyledLink href="/teams/create">
+        <StyledButton>{t('Create a new team')}</StyledButton>
+      </StyledLink>
+    </Box>
+  );
+};
+
+Page.getLayout = function getLayout(page: ReactElement) {
+  return <TeamLayout>{page}</TeamLayout>;
+};
+
+export default Page;
