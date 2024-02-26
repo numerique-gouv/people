81243cfc9a7e3754027f011a3d34aad3307aff66
Marie PUPO JEAMMET <marie.jeammet@beta.gouv.fr>
2024-02-26 18:03:31 +0100

    ✨(api) return user id, name and email on /team/<id>/accesses/
    Add serializers to return basic user info when listing /team/<id>/accesses/
    endpoint. This will allow front-end to retrieve members info without having
    to query API for each user.id.

--- src/backend/core/tests/test_api_users.py
+++ src/backend/core/tests/test_api_users.py
@@ -58,10 +58,10 @@ def test_api_users_authenticated_list_by_email():
     client = APIClient()
     client.force_login(user)
 
-    dave = factories.IdentityFactory(email="david.bowman@work.com")
-    nicole = factories.IdentityFactory(email="nicole_foole@work.com")
-    frank = factories.IdentityFactory(email="frank_poole@work.com")
-    factories.IdentityFactory(email="heywood_floyd@work.com")
+    dave = factories.IdentityFactory(email="david.bowman@work.com", is_main=True)
+    nicole = factories.IdentityFactory(email="nicole_foole@work.com", is_main=True)
+    frank = factories.IdentityFactory(email="frank_poole@work.com", is_main=True)
+    factories.IdentityFactory(email="heywood_floyd@work.com", is_main=True)
 
     # Full query should work
     response = client.get(
@@ -91,8 +91,26 @@ def test_api_users_authenticated_list_by_email():
     response = client.get("/api/v1.0/users/?q=ool")
 
     assert response.status_code == HTTP_200_OK
-    user_ids = [user["id"] for user in response.json()["results"]]
-    assert user_ids == [str(nicole.user.id), str(frank.user.id)]
+    assert response.json()["results"] == [
+        {
+            "id": str(nicole.user.id),
+            "email": nicole.email,
+            "name": nicole.name,
+            "is_device": nicole.user.is_device,
+            "is_staff": nicole.user.is_staff,
+            "language": nicole.user.language,
+            "timezone": str(nicole.user.timezone),
+        },
+        {
+            "id": str(frank.user.id),
+            "email": frank.email,
+            "name": frank.name,
+            "is_device": frank.user.is_device,
+            "is_staff": frank.user.is_staff,
+            "language": frank.user.language,
+            "timezone": str(frank.user.timezone),
+        },
+    ]
 
 
 def test_api_users_authenticated_list_multiple_identities_single_user():
@@ -132,12 +150,16 @@ def test_api_users_authenticated_list_multiple_identities_multiple_users():
     client.force_login(user)
 
     dave = factories.UserFactory()
-    davina = factories.UserFactory()
-    prudence = factories.UserFactory()
-    factories.IdentityFactory(user=dave, email="david.bowman@work.com")
+    dave_identity = factories.IdentityFactory(
+        user=dave, email="david.bowman@work.com", is_main=True
+    )
     factories.IdentityFactory(user=dave, email="babibou@ehehe.com")
-    factories.IdentityFactory(user=davina, email="davina.bowan@work.com")
-    factories.IdentityFactory(user=prudence, email="prudence.crandall@work.com")
+    davina_identity = factories.IdentityFactory(
+        user=factories.UserFactory(), email="davina.bowan@work.com"
+    )
+    prue_identity = factories.IdentityFactory(
+        user=factories.UserFactory(), email="prudence.crandall@work.com"
+    )
 
     # Full query should work
     response = client.get(
@@ -146,8 +168,35 @@ def test_api_users_authenticated_list_multiple_identities_multiple_users():
 
     assert response.status_code == HTTP_200_OK
     assert response.json()["count"] == 3
-    user_ids = [user["id"] for user in response.json()["results"]]
-    assert user_ids == [str(dave.id), str(davina.id), str(prudence.id)]
+    assert response.json()["results"] == [
+        {
+            "id": str(dave.id),
+            "email": dave_identity.email,
+            "name": dave_identity.name,
+            "is_device": dave_identity.user.is_device,
+            "is_staff": dave_identity.user.is_staff,
+            "language": dave_identity.user.language,
+            "timezone": str(dave_identity.user.timezone),
+        },
+        {
+            "id": str(davina_identity.user.id),
+            "email": davina_identity.email,
+            "name": davina_identity.name,
+            "is_device": davina_identity.user.is_device,
+            "is_staff": davina_identity.user.is_staff,
+            "language": davina_identity.user.language,
+            "timezone": str(davina_identity.user.timezone),
+        },
+        {
+            "id": str(prue_identity.user.id),
+            "email": prue_identity.email,
+            "name": prue_identity.name,
+            "is_device": prue_identity.user.is_device,
+            "is_staff": prue_identity.user.is_staff,
+            "language": prue_identity.user.language,
+            "timezone": str(prue_identity.user.timezone),
+        },
+    ]
 
 
 def test_api_users_authenticated_list_uppercase_content():
@@ -291,7 +340,7 @@ def test_api_users_retrieve_me_anonymous():
 
 def test_api_users_retrieve_me_authenticated():
     """Authenticated users should be able to retrieve their own user via the "/users/me" path."""
-    identity = factories.IdentityFactory()
+    identity = factories.IdentityFactory(is_main=True)
     user = identity.user
 
     client = APIClient()
@@ -310,7 +359,8 @@ def test_api_users_retrieve_me_authenticated():
     assert response.status_code == HTTP_200_OK
     assert response.json() == {
         "id": str(user.id),
-        "email": str(user.email),
+        "name": str(identity.name),
+        "email": str(identity.email),
         "language": user.language,
         "timezone": str(user.timezone),
         "is_device": False,
