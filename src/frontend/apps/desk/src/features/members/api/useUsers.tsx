2295a7ea830e6bced200126b09416a8eee24f29c
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-03-14 15:47:56 +0100

    ✨(app-desk) add useUsers react-query hook
    Add the hook useUsers, it will be used to
    search users by name or email.

---
+++ src/frontend/apps/desk/src/features/members/api/useUsers.tsx
@@ -0,0 +1,36 @@
+import { UseQueryOptions, useQuery } from '@tanstack/react-query';
+
+import { APIError, APIList, errorCauses, fetchAPI } from '@/api';
+import { User } from '@/features/auth';
+
+export type UsersParams = {
+  query: string;
+};
+
+type UsersResponse = APIList<User>;
+
+export const getUsers = async ({
+  query,
+}: UsersParams): Promise<UsersResponse> => {
+  const queryParam = query ? `q=${query}` : '';
+  const response = await fetchAPI(`users/?${queryParam}`);
+
+  if (!response.ok) {
+    throw new APIError('Failed to get the users', await errorCauses(response));
+  }
+
+  return response.json() as Promise<UsersResponse>;
+};
+
+export const KEY_LIST_USER = 'users';
+
+export function useUsers(
+  param: UsersParams,
+  queryConfig?: UseQueryOptions<UsersResponse, APIError, UsersResponse>,
+) {
+  return useQuery<UsersResponse, APIError, UsersResponse>({
+    queryKey: [KEY_LIST_USER, param],
+    queryFn: () => getUsers(param),
+    ...queryConfig,
+  });
+}
