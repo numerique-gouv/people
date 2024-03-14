5af8d0ac7b9586e1af7cbc104a220409f184d693
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-03-14 15:44:20 +0100

    ✨(app-desk) integrate multiselect search users
    Integrate multiselect search users in the
    modal add members.
    We are using react-select to implement the
    multiselect search users. We are using this
    library in waiting for Cunningham to implement
    the multiselect asynch component.

--- src/frontend/apps/desk/src/features/members/components/ModalAddMembers.tsx
+++ src/frontend/apps/desk/src/features/members/components/ModalAddMembers.tsx
@@ -1,28 +1,40 @@
 import { Button, Modal, ModalSize } from '@openfun/cunningham-react';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { createGlobalStyle } from 'styled-components';
 
-import { StyledLink } from '@/components';
+import { Box, StyledLink } from '@/components';
+import { Team } from '@/features/teams';
+
+import { OptionSelect, SearchMembers } from './SearchMembers';
+
+const GlobalStyle = createGlobalStyle`
+  .c__modal {
+    overflow: visible;
+  }
+`;
 
 interface ModalAddMembersProps {
-  teamId: string;
+  team: Team;
 }
 
-export const ModalAddMembers = ({ teamId }: ModalAddMembersProps) => {
+export const ModalAddMembers = ({ team }: ModalAddMembersProps) => {
   const { t } = useTranslation();
   const router = useRouter();
+  const [selectedMembers, setSelectedMembers] = useState<OptionSelect>([]);
 
   return (
     <Modal
       isOpen
       leftActions={
-        <StyledLink href={`/teams/${teamId}/`}>
+        <StyledLink href={`/teams/${team.id}/`}>
           <Button color="secondary" fullWidth>
             {t('Cancel')}
           </Button>
         </StyledLink>
       }
-      onClose={() => void router.push(`/teams/${teamId}/`)}
+      onClose={() => void router.push(`/teams/${team.id}/`)}
       rightActions={
         <Button color="primary" fullWidth onClick={() => {}}>
           {t('Validate')}
@@ -30,6 +42,11 @@ export const ModalAddMembers = ({ teamId }: ModalAddMembersProps) => {
       }
       size={ModalSize.MEDIUM}
       title={t('Add members to the team')}
-    ></Modal>
+    >
+      <GlobalStyle />
+      <Box className="mb-xl mt-l">
+        <SearchMembers team={team} setSelectedMembers={setSelectedMembers} />
+      </Box>
+    </Modal>
   );
 };
---
+++ src/frontend/apps/desk/src/features/members/components/SearchMembers.tsx
@@ -0,0 +1,109 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Options } from 'react-select';
+import AsyncSelect from 'react-select/async';
+
+import { User } from '@/features/auth';
+import { Team } from '@/features/teams';
+
+import { KEY_LIST_USER, useUsers } from '../api/useUsers';
+
+const isValidEmail = (email: string) => {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  );
+};
+
+export type OptionSelect = Options<{
+  value: Partial<User> & { email: User['email'] };
+  label: string;
+}>;
+
+interface SearchMembersProps {
+  team: Team;
+  setSelectedMembers: (value: OptionSelect) => void;
+}
+
+export const SearchMembers = ({
+  team,
+  setSelectedMembers,
+}: SearchMembersProps) => {
+  const { t } = useTranslation();
+  const [input, setInput] = useState('');
+  const [userQuery, setUserQuery] = useState('');
+  const resolveOptionsRef = useRef<((value: OptionSelect) => void) | null>(
+    null,
+  );
+  const { data } = useUsers(
+    { query: userQuery },
+    {
+      enabled: !!userQuery,
+      queryKey: [KEY_LIST_USER, { query: userQuery }],
+    },
+  );
+
+  const options = data?.results;
+
+  useEffect(() => {
+    if (resolveOptionsRef.current && options) {
+      let users: OptionSelect = options.map((user) => ({
+        value: user,
+        label: user.name || '',
+      }));
+
+      if (userQuery && isValidEmail(userQuery)) {
+        const isFound = !!options.find(
+          (user) => user.email === userQuery || user.name === userQuery,
+        );
+
+        if (!isFound) {
+          users = [
+            {
+              value: { email: userQuery },
+              label: userQuery,
+            },
+          ];
+        }
+      }
+
+      resolveOptionsRef.current(users);
+      resolveOptionsRef.current = null;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [options]);
+
+  const loadOptions = (): Promise<OptionSelect> => {
+    return new Promise<OptionSelect>((resolve) => {
+      resolveOptionsRef.current = resolve;
+    });
+  };
+
+  const timeout = useRef<NodeJS.Timeout | null>(null);
+  const onInputChangeHandle = useCallback((newValue: string) => {
+    setInput(newValue);
+    if (timeout.current) {
+      clearTimeout(timeout.current);
+    }
+
+    timeout.current = setTimeout(() => {
+      setUserQuery(newValue);
+    }, 1000);
+  }, []);
+
+  return (
+    <AsyncSelect
+      isMulti
+      loadOptions={loadOptions}
+      defaultOptions={[]}
+      onInputChange={onInputChangeHandle}
+      inputValue={input}
+      placeholder={t('Search new members (name or email)')}
+      noOptionsMessage={() =>
+        t('Invite new members to {{teamName}}', { teamName: team.name })
+      }
+      onChange={(value) => {
+        setSelectedMembers(value);
+      }}
+    />
+  );
+};
