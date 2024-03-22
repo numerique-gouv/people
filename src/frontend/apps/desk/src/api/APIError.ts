bbf2695dee46c06d1791436157fb1ba3943c6d87
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-03-22 15:59:50 +0100

    🥅(app-desk) add data to APIError
    We sometime need to get some data back when an
    error occurs. We add a data prop to the APIError
    class to allow us to do that.

--- src/frontend/apps/desk/src/api/APIError.ts
+++ src/frontend/apps/desk/src/api/APIError.ts
@@ -1,17 +1,20 @@
-interface IAPIError {
+interface IAPIError<T = unknown> {
   status: number;
   cause?: string[];
+  data?: T;
 }
 
-export class APIError extends Error implements IAPIError {
-  public status: number;
-  public cause?: string[];
+export class APIError<T = unknown> extends Error implements IAPIError<T> {
+  public status: IAPIError['status'];
+  public cause?: IAPIError['cause'];
+  public data?: IAPIError<T>['data'];
 
-  constructor(message: string, { status, cause }: IAPIError) {
+  constructor(message: string, { status, cause, data }: IAPIError<T>) {
     super(message);
 
     this.name = 'APIError';
     this.status = status;
     this.cause = cause;
+    this.data = data;
   }
 }
