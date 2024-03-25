57af4f3d8aaa8010a6067bb025b40db667eac82f
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-03-25 17:49:11 +0100

    🚚(app-desk) create addMembers feature
    All the code related to adding members has been moved
    to the addMembers feature.

--- src/frontend/apps/desk/src/features/members/index.ts
+++ src/frontend/apps/desk/src/features/members/index.ts
@@ -1,3 +1,4 @@
-export * from './types';
+export * from './api/useTeamsAccesses';
+export * from './components/ChooseRole';
 export * from './components/MemberGrid';
-export * from './components/ModalAddMembers';
+export * from './types';
