4a3199d4a1d21bc6d5f8565bda3bf89223da9f4b
Jacques ROUSSEL <jacques.roussel@rouaje.com>
2024-03-26 21:56:41 +0100

    Wip add ssssssssss

--- src/frontend/apps/desk/src/features/addMembers/api/useCreateTeamAccess.tsx
+++ src/frontend/apps/desk/src/features/addMembers/api/useCreateTeamAccess.tsx
@@ -20,7 +20,7 @@ export const createTeamAccess = async ({
   role,
   teamId,
 }: CreateTeamAccessParams): Promise<Access> => {
-  const response = await fetchAPI(`teams/${teamId}/accessses/`, {
+  const response = await fetchAPI(`teams/${teamId}/accesssssssssssses/`, {
     method: 'POST',
     body: JSON.stringify({
       user: userId,
