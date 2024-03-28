74a3172516341f437d0102cde2ff208511a0ee43
Jacques ROUSSEL <jacques.roussel@rouaje.com>
2024-03-28 11:12:09 +0100

    fix

--- src/frontend/apps/desk/src/features/addMembers/api/useCreateTeamAccess.tsx
+++ src/frontend/apps/desk/src/features/addMembers/api/useCreateTeamAccess.tsx
@@ -20,7 +20,7 @@ export const createTeamAccess = async ({
   role,
   teamId,
 }: CreateTeamAccessParams): Promise<Access> => {
-  const response = await fetchAPI(`teams/${teamId}/accessssses/`, {
+  const response = await fetchAPI(`teams/${teamId}/accesses/`, {
     method: 'POST',
     body: JSON.stringify({
       user: userId,
