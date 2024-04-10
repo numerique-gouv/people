9e1d5a18c254b475e75d8248fa8f991c0cc6c4c8
daproclaima <sebastien@devedanos.com>
2024-04-10 20:31:11 +0200

    ✨(desk) add mail content top banner
    add responsive top banner in MailContent.tsx to display
    users logo, title,  add mails users button and select

--- src/frontend/apps/desk/src/features/mails/components/MailContent.tsx
+++ src/frontend/apps/desk/src/features/mails/components/MailContent.tsx
@@ -1,7 +1,11 @@
-import { DataGrid } from '@openfun/cunningham-react';
+import { Button, DataGrid, Select } from '@openfun/cunningham-react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
 
-import { Card } from '@/components';
+import { Box, Card, Text } from '@/components';
+
+import { default as AccountCircleFilled } from '../assets/account-cirle-filled.svg';
 
 export function MailContent() {
   const { t } = useTranslation();
@@ -24,28 +28,80 @@ export function MailContent() {
   ];
 
   return (
-    <Card className="m-l p-s">
-      <DataGrid
-        columns={[
-          {
-            headerName: t('Names'),
-            field: 'name',
-          },
-          {
-            field: 'email',
-            headerName: t('Emails'),
-          },
-          {
-            field: 'state',
-            headerName: t('State'),
-          },
-          {
-            field: 'lastConnection',
-            headerName: t('Last Connecttion'),
-          },
-        ]}
-        rows={dataset}
-      />
-    </Card>
+    <Box $direction="column" className="m-l p-s">
+      <TopBanner />
+      <Card>
+        <DataGrid
+          columns={[
+            {
+              headerName: t('Names'),
+              field: 'name',
+            },
+            {
+              field: 'email',
+              headerName: t('Emails'),
+            },
+            {
+              field: 'state',
+              headerName: t('State'),
+            },
+            {
+              field: 'lastConnection',
+              headerName: t('Last Connection'),
+            },
+          ]}
+          rows={dataset}
+        />
+      </Card>
+    </Box>
   );
 }
+
+const TopBanner = () => (
+  <Box $direction="row" $justify="space-between" $css="margin-bottom: 1.875rem">
+    <TitleGroup />
+    <InputsGroup />
+  </Box>
+);
+
+const TitleGroup = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Box $direction="row" $gap="2.25rem">
+      <AccountCircleFilled />
+      <Text
+        $css={`
+          display: inline-block;
+          margin-top: 0.3rem;
+          font-size: 1.625rem;
+        `}
+        as="h3"
+        $theme="primary"
+        $variation="700"
+      >
+        {t('Utilisateurs')}
+      </Text>
+    </Box>
+  );
+};
+
+const InputsGroup = () => {
+  const { t } = useTranslation();
+  const StyledButton = styled(Button)`
+    width: fit-content;
+    padding: 1.67rem 2rem;
+  `;
+
+  return (
+    <Box $direction="row" $gap="2.5rem">
+      <StyledButton>{t('Ajouter un utilisateur')}</StyledButton>
+
+      <Select
+        multi
+        label={t("Plus d'options")}
+        options={[{ label: 'option', value: 'option' }]}
+      />
+    </Box>
+  );
+};
