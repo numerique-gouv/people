1ea1ce796ac219c77fc741dfe1e18496fc8b8670
antoine lebaud <antoine.lebaud@mail.numerique.gouv.fr>
2024-04-23 00:28:51 +0200

    ♻️(backend) encapsulate cryptographic key loading
    I explained the responsibilities of the private key in the context of the
    resource server.
    
    Consider enhancing this utility function, as it currently imposes an
    opinionated approach to loading the private key and passing parameters to the
    joserfc utilities.
    
    The documentation could be more concise to avoid excessive detail.
    
    The current API and return type are tightly coupled with joserfc usage and
    types. Should we consider exposing a generic Python object to simplify the
    interface?

--- src/backend/core/resource_server/views.py
+++ src/backend/core/resource_server/views.py
@@ -1,11 +1,10 @@
 """Resource Server views"""
 
-from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
-
-from joserfc.jwk import JWKRegistry, KeySet
+from joserfc.jwk import KeySet
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from .utils import import_private_key_from_settings
 
 
 class JWKSView(APIView):
@@ -26,26 +25,7 @@ class JWKSView(APIView):
             Response: JSON response containing the JWK data.
         """
 
-        private_key_str = settings.RESOURCE_SERVER_JWK_PRIVATE_KEY_STR
-
-        if not private_key_str:
-            raise ImproperlyConfigured(
-                "RESOURCE_SERVER_JWK_PRIVATE_KEY_STR setting is missing or empty."
-            )
-
-        private_key_pem = private_key_str.encode()
-
-        try:
-            private_key = JWKRegistry.import_key(
-                private_key_pem,
-                key_type=settings.RESOURCE_SERVER_JWK_KEY_TYPE,
-                parameters={"alg": settings.RESOURCE_SERVER_JWK_ALG, "use": "enc"},
-            )
-        except ValueError as err:
-            raise ImproperlyConfigured(
-                "RESOURCE_SERVER_JWK_PRIVATE_KEY_STR setting is wrong."
-            ) from err
-
+        private_key = import_private_key_from_settings()
         jwk = KeySet([private_key]).as_dict()
 
         return Response(jwk)
