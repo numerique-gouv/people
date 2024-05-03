c599757d7a5ee93c5eed1e476634e7a9dea7be95
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-05-03 11:20:44 +0200

    🗑️(app-desk) clean the menu
    - remove unused icons
    - remove unused pages
    - remove menu items

--- src/frontend/apps/desk/src/features/menu/Menu.tsx
+++ src/frontend/apps/desk/src/features/menu/Menu.tsx
@@ -6,11 +6,7 @@ import { Box } from '@/components/';
 import useCunninghamTheme from '@/cunningham/useCunninghamTheme';
 
 import MenuItem from './MenuItems';
-import IconRecent from './assets/icon-clock.svg';
-import IconContacts from './assets/icon-contacts.svg';
 import IconMail from './assets/icon-mails.svg';
-import IconSearch from './assets/icon-search.svg';
-import IconFavorite from './assets/icon-stars.svg';
 
 export const Menu = () => {
   const { colorsTokens } = useCunninghamTheme();
@@ -26,12 +22,8 @@ export const Menu = () => {
       $margin="none"
     >
       <Box $padding={{ top: 'large' }} $direction="column" $gap="0.8rem">
-        <MenuItem Icon={IconSearch} label={t('Search')} href="/" />
+        <MenuItem Icon={IconGroup} label={t('Teams')} href="/" />
         <MenuItem Icon={IconMail} label={t('Mails')} href="/mails" />
-        <MenuItem Icon={IconFavorite} label={t('Favorite')} href="/favorite" />
-        <MenuItem Icon={IconRecent} label={t('Recent')} href="/recent" />
-        <MenuItem Icon={IconContacts} label={t('Contacts')} href="/contacts" />
-        <MenuItem Icon={IconGroup} label={t('Groups')} href="/groups" />
       </Box>
     </Box>
   );
