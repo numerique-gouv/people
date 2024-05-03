01d72466a68d51d6e5cfde0ab72fb3628e47489d
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-05-03 10:28:27 +0200

    ✨(app-desk) add footer
    Add footer to the desk app.

--- src/frontend/apps/desk/src/core/MainLayout.tsx
+++ src/frontend/apps/desk/src/core/MainLayout.tsx
@@ -1,19 +1,27 @@
 import { PropsWithChildren } from 'react';
 
 import { Box } from '@/components';
+import { Footer } from '@/features/footer/Footer';
 import { HEADER_HEIGHT, Header } from '@/features/header';
 import { Menu } from '@/features/menu';
 
 export function MainLayout({ children }: PropsWithChildren) {
   return (
-    <Box $height="100vh" $css="overflow:hidden;">
-      <Header />
-      <Box $css="flex: 1;" $direction="row">
-        <Menu />
-        <Box as="main" $height={`calc(100vh - ${HEADER_HEIGHT})`} $width="100%">
-          {children}
+    <Box>
+      <Box $height="100vh">
+        <Header />
+        <Box $css="flex: 1;" $direction="row">
+          <Menu />
+          <Box
+            as="main"
+            $height={`calc(100vh - ${HEADER_HEIGHT})`}
+            $width="100%"
+          >
+            {children}
+          </Box>
         </Box>
       </Box>
+      <Footer />
     </Box>
   );
 }
