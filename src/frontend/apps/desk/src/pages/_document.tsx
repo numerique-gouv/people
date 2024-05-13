1f4a81b26a16f1ff069159cf4bb48ab7e91d0f02
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-05-13 16:19:48 +0200

    ♻️(app-desk) adapt La Gaufre
    Adapt La Gaufre with the new configuration.

--- src/frontend/apps/desk/src/pages/_document.tsx
+++ src/frontend/apps/desk/src/pages/_document.tsx
@@ -2,21 +2,10 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 import '@/i18n/initI18n';
 
-/**
- * About the Gaufre Vanilla CSS
- * To respect next.js standards, the css is included here.
- * This css is used with features/header/ApplicationsMenu component.
- * If the ApplicationsMenu component is removed, this css can be removed as well.
- */
 export default function RootLayout() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://suite-numerique-gaufre.osc-fr1.scalingo.io/public/styles/gaufre-vanilla.css"
-        />
-      </Head>
+      <Head />
       <body suppressHydrationWarning={process.env.NODE_ENV === 'development'}>
         <Main />
         <NextScript />
