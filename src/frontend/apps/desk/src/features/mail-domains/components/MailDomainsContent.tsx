37d32888f50bc3c2ec80c492d9e40b77b966c1fc
daproclaima <sebastien@devedanos.com>
2024-05-21 16:25:19 +0200

    ✨(app-desk) displays specific mail domain mailboxes
    Fetches a mail domain by id and displays
    its mailboxes as a list in a table.
    Associated with e2e tests.

--- src/frontend/apps/desk/src/features/mail-domains/components/MailDomainsContent.tsx
+++ src/frontend/apps/desk/src/features/mail-domains/components/MailDomainsContent.tsx
@@ -1,51 +1,123 @@
-import { DataGrid } from '@openfun/cunningham-react';
+import {
+  DataGrid,
+  Loader,
+  SortModel,
+  usePagination,
+} from '@openfun/cunningham-react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { Card } from '@/components';
+import { Box, Card, Text, TextErrors } from '@/components';
+import { MailDomain } from '@/features/mail-domains';
+import { useMailDomainMailboxes } from '@/features/mail-domains/api/useMailDomainMailboxes';
+import { PAGE_SIZE } from '@/features/mail-domains/conf';
 
-export function MailDomainsContent() {
+import { default as MailDomainsLogo } from '../assets/mail-domains-logo.svg';
+
+export type ViewMailbox = { email: string; id: string };
+
+// FIXME : ask Cunningham to export this type
+type SortModelItem = {
+  field: string;
+  sort: 'asc' | 'desc' | null;
+};
+
+const defaultOrderingMapping: Record<string, string> = {
+  email: 'local_part',
+};
+
+/**
+ * Formats the sorting model based on a given mapping.
+ * @param {SortModelItem} sortModel The sorting model item containing field and sort direction.
+ * @param {Record<string, string>} mapping The mapping object to map field names.
+ * @returns {string} The formatted sorting string.
+ */
+function formatSortModel(
+  sortModel: SortModelItem,
+  mapping = defaultOrderingMapping,
+) {
+  const { field, sort } = sortModel;
+  const orderingField = mapping[field] || field;
+  return sort === 'desc' ? `-${orderingField}` : orderingField;
+}
+
+export function MailDomainsContent({ mailDomain }: { mailDomain: MailDomain }) {
+  const [sortModel, setSortModel] = useState<SortModel>([]);
   const { t } = useTranslation();
+  const pagination = usePagination({
+    defaultPage: 1,
+    pageSize: PAGE_SIZE,
+  });
 
-  const dataset = [
-    {
-      id: '1',
-      name: 'John Doe',
-      email: 'john@doe.com',
-      state: 'Active',
-      lastConnection: '2021-09-01',
-    },
-    {
-      id: '2',
-      name: 'Jane Doe',
-      email: 'jane@doe.com',
-      state: 'Inactive',
-      lastConnection: '2021-09-02',
-    },
-  ];
+  const { page, pageSize, setPagesCount } = pagination;
+  const ordering = sortModel.length ? formatSortModel(sortModel[0]) : undefined;
 
-  return (
-    <Card $padding="small" $margin="large">
-      <DataGrid
-        columns={[
-          {
-            headerName: t('Names'),
-            field: 'name',
-          },
-          {
-            field: 'email',
-            headerName: t('Emails'),
-          },
-          {
-            field: 'state',
-            headerName: t('State'),
-          },
-          {
-            field: 'lastConnection',
-            headerName: t('Last Connecttion'),
-          },
-        ]}
-        rows={dataset}
-      />
-    </Card>
+  const { data, isLoading, error } = useMailDomainMailboxes({
+    id: mailDomain.id,
+    page,
+    ordering,
+  });
+
+  const viewMailboxes: ViewMailbox[] =
+    mailDomain && data?.results?.length
+      ? data.results.map((mailbox) => ({
+          email: `${mailbox.local_part}@${mailDomain.name}`,
+          id: mailbox.id,
+        }))
+      : [];
+
+  useEffect(() => {
+    setPagesCount(data?.count ? Math.ceil(data.count / pageSize) : 0);
+  }, [data?.count, pageSize, setPagesCount]);
+  return isLoading ? (
+    <Box $align="center" $justify="center" $height="100%">
+      <Loader />
+    </Box>
+  ) : (
+    <>
+      <TopBanner name={mailDomain.name} />
+      <Card
+        $padding={{ bottom: 'small' }}
+        $margin={{ all: 'big', top: 'none' }}
+        $overflow="auto"
+      >
+        {error && <TextErrors causes={error.cause} />}
+        <DataGrid
+          columns={[
+            {
+              field: 'email',
+              headerName: t('Emails'),
+            },
+          ]}
+          rows={viewMailboxes}
+          isLoading={isLoading}
+          onSortModelChange={setSortModel}
+          sortModel={sortModel}
+          pagination={{
+            ...pagination,
+            displayGoto: false,
+          }}
+          aria-label={t('Mailboxes list')}
+        />
+      </Card>
+    </>
   );
 }
+
+const TopBanner = ({ name }: { name: string }) => {
+  const { t } = useTranslation();
+
+  return (
+    <Box
+      $direction="row"
+      $align="center"
+      $margin={{ all: 'big', vertical: 'xbig' }}
+      $gap="2.25rem"
+    >
+      <MailDomainsLogo aria-label={t('Mail Domains icon')} />
+      <Text $margin="none" as="h3" $size="h3">
+        {name}
+      </Text>
+    </Box>
+  );
+};
