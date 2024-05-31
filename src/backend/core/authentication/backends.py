numerique-gouv/impress

🚚(backend) create a dedicated authentication package
Prepare adding advanced authentication features. Create a dedicated
authentication Python package within the core app.

This code organization will be more extensible.

src/backend/core/authentication/backends.py
@@ -1,4 +1,4 @@
-"""Authentication for the Impress core app."""
+"""Authentication Backends for the Impress core app."""
 
 from django.core.exceptions import SuspiciousOperation
 from django.utils.translation import gettext_lazy as _
@@ -8,7 +8,7 @@ from mozilla_django_oidc.auth import (
     OIDCAuthenticationBackend as MozillaOIDCAuthenticationBackend,
 )
 
-from .models import User
+from core.models import User
 
 
 class OIDCAuthenticationBackend(MozillaOIDCAuthenticationBackend):

numerique-gouv/people

src/backend/core/authentication/backends.py
@@ -1,4 +1,4 @@
-"""Authentication for the People core app."""
+"""Authentication Backends for the People core app."""
 
 from django.conf import settings
 from django.core.exceptions import SuspiciousOperation
@@ -10,7 +10,7 @@ from mozilla_django_oidc.auth import (
     OIDCAuthenticationBackend as MozillaOIDCAuthenticationBackend,
 )
 
-from .models import Identity
+from core.models import Identity
 
 
 class OIDCAuthenticationBackend(MozillaOIDCAuthenticationBackend):