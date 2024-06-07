6e7f20eda9eba060e8e27498064f1e4f5a8f6ce4
Jacques ROUSSEL <jacques.roussel@rouaje.com>
2024-06-07 08:56:23 +0200

    💚(ci) remove secret from repository
    - Remove *.enc.*
    - Adapt helmfile
    - Adapt CI

---
+++ scripts/update-git-submodule.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git submodule update --init --recursive
+git submodule foreach 'git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); git submodule update --recursive; git clean -dfx'
