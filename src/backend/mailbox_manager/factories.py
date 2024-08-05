2c82f38c5936d6f0c5c15e1379c2352d8201e0dc
Marie PUPO JEAMMET <marie.jeammet@beta.gouv.fr>
2024-08-05 12:13:13 +0200

    🗃️(domains) add "secret" field to domains
    add a "secret" field to domain model. This secret will be used as
    password in mail provisioning API.

--- src/backend/mailbox_manager/factories.py
+++ src/backend/mailbox_manager/factories.py
@@ -27,6 +27,7 @@ class MailDomainFactory(factory.django.DjangoModelFactory):
 
     name = factory.Faker("domain_name")
     slug = factory.LazyAttribute(lambda o: slugify(o.name))
+    secret = factory.Faker("password")
 
     @factory.post_generation
     def users(self, create, extracted, **kwargs):
