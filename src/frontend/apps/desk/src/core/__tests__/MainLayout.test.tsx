596db08f56d875133ee3c52c45d8ddb86be0a20c
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-08-08 15:55:31 +0200

    🔥(frontend) remove temporarily team feature
    We want to make a first realease, but the
    team feature is not ready yet.
    So we will hide it for now by hiding the menu.
    We will still let the feature in dev environment.

---
+++ src/frontend/apps/desk/src/core/__tests__/MainLayout.test.tsx
@@ -0,0 +1,47 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
+import { AppWrapper } from '@/tests/utils';
+
+import { MainLayout } from '../MainLayout';
+
+jest.mock('next/navigation', () => ({
+  ...jest.requireActual('next/navigation'),
+  usePathname: () => '/',
+}));
+
+describe('MainLayout', () => {
+  it('checks menu rendering', () => {
+    render(<MainLayout />, { wrapper: AppWrapper });
+
+    expect(
+      screen.getByRole('button', {
+        name: /Teams button/i,
+      }),
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('button', {
+        name: /Mail Domains button/i,
+      }),
+    ).toBeInTheDocument();
+  });
+
+  it('checks menu rendering without team feature', () => {
+    process.env.NEXT_PUBLIC_FEATURE_TEAM = 'false';
+
+    render(<MainLayout />, { wrapper: AppWrapper });
+
+    expect(
+      screen.queryByRole('button', {
+        name: /Teams button/i,
+      }),
+    ).not.toBeInTheDocument();
+
+    expect(
+      screen.queryByRole('button', {
+        name: /Mail Domains button/i,
+      }),
+    ).not.toBeInTheDocument();
+  });
+});
