25d22389d8866328368894ac827a70ee451a5051
daproclaima <sebastien@devedanos.com>
2024-09-04 22:03:43 +0200

    🥅(frontend) handle api errors
    - add hook to handle api errors.
    - add related component tests

---
+++ src/frontend/apps/desk/src/api/parseAPIError.ts
@@ -0,0 +1,83 @@
+import { APIError } from '@/api/index';
+
+type ErrorParams = {
+  [fieldName: string]: {
+    causes: string[];
+    causeShown?: string;
+    handleError: () => void;
+  };
+};
+
+type ServerErrorParams = {
+  defaultMessage: string;
+  handleError?: () => void;
+};
+
+export type parseAPIErrorParams = {
+  error: APIError | null;
+  errorParams?: ErrorParams;
+  serverErrorParams: ServerErrorParams;
+};
+export const parseAPIError = ({
+  error,
+  errorParams,
+  serverErrorParams,
+}: parseAPIErrorParams) => {
+  if (!error || !serverErrorParams?.defaultMessage) {
+    return;
+  }
+
+  let causes: string[] =
+    error.cause?.length && errorParams
+      ? parseAPIErrorCause({ causes: error.cause, errorParams })
+      : [];
+
+  if (error?.status === 500 || !error?.status) {
+    causes = parseServerAPIError({ causes, serverErrorParams });
+  }
+
+  return causes;
+};
+
+export const parseAPIErrorCause = ({
+  causes,
+  errorParams,
+}: {
+  causes: string[];
+  errorParams: ErrorParams;
+}): string[] =>
+  causes.reduce((arrayCauses, cause) => {
+    const foundErrorParams = Object.values(errorParams).find((params) =>
+      params.causes.find((knownCause) => knownCause.match(cause)),
+    );
+
+    if (!foundErrorParams) {
+      arrayCauses.push(cause);
+    }
+
+    if (foundErrorParams?.causeShown) {
+      arrayCauses.push(foundErrorParams.causeShown);
+    }
+
+    if (typeof foundErrorParams?.handleError === 'function') {
+      foundErrorParams.handleError();
+    }
+
+    return arrayCauses;
+  }, [] as string[]);
+
+export const parseServerAPIError = ({
+  causes,
+  serverErrorParams,
+}: {
+  causes: string[];
+  serverErrorParams: ServerErrorParams;
+}): string[] => {
+  causes.unshift(serverErrorParams.defaultMessage);
+
+  if (typeof serverErrorParams?.handleError === 'function') {
+    serverErrorParams.handleError();
+  }
+
+  return causes;
+};
