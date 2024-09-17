e9b00755033d6af6ce330ce00be0c210d81dc38b
daproclaima <sebastien@devedanos.com>
2024-09-17 15:42:04 +0200

    ⚗️(frontend) show username on AccountDropDown
    - show username instead of "My account"

--- src/frontend/apps/desk/src/features/header/AccountDropdown.tsx
+++ src/frontend/apps/desk/src/features/header/AccountDropdown.tsx
@@ -7,32 +7,55 @@ import { useAuthStore } from '@/core/auth';
 
 export const AccountDropdown = () => {
   const { t } = useTranslation();
-  const { logout } = useAuthStore();
+  const { userData, logout } = useAuthStore();
 
+  console.log('userData: ', userData);
+  const userName = userData?.name || t('No Username');
   return (
     <DropButton
-      aria-label={t('My account')}
+      aria-label={userName}
       button={
         <Box $flex $direction="row" $align="center">
-          <Text $theme="primary">{t('My account')}</Text>
+          <Text $theme="primary">{userName}</Text>
           <Text className="material-icons" $theme="primary" aria-hidden="true">
             arrow_drop_down
           </Text>
         </Box>
       }
     >
-      <Button
-        onClick={logout}
-        color="primary-text"
-        icon={
-          <span className="material-icons" aria-hidden="true">
-            logout
-          </span>
-        }
-        aria-label={t('Logout')}
-      >
-        <Text $weight="normal">{t('Logout')}</Text>
-      </Button>
+      <Box $css="display: flex; direction: column; gap: 0.5rem">
+        {/*<Box>*/}
+        {/*  <Button*/}
+        {/*    key="account"*/}
+        {/*    color="primary-text"*/}
+        {/*    icon={*/}
+        {/*      <span className="material-icons" aria-hidden="true">*/}
+        {/*        person*/}
+        {/*      </span>*/}
+        {/*    }*/}
+        {/*    aria-label={t('My account')}*/}
+        {/*  >*/}
+        {/*    <Text $weight="normal">{t('My account')}</Text>*/}
+        {/*  </Button>*/}
+        {/*</Box>*/}
+        {/*<Box $css="border-bottom: 1px var(--c--theme--colors--greyscale-400) solid; width: 100%; border-radius: 2px" />*/}
+
+        <Box>
+          <Button
+            onClick={logout}
+            key="logout"
+            color="primary-text"
+            icon={
+              <span className="material-icons" aria-hidden="true">
+                logout
+              </span>
+            }
+            aria-label={t('Logout')}
+          >
+            <Text $weight="normal">{t('Logout')}</Text>
+          </Button>
+        </Box>
+      </Box>
     </DropButton>
   );
 };
