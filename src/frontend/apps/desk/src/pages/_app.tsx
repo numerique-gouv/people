e0d844a7e64cc65de29617bdfbd8698d32bfbe6a
NathanPanchout <nathanpanchout@hotmail.com>
2024-10-15 09:23:27 +0200

    💄(frontend) use sass
    use sass in global.scss file and modify import in _app.tsx file

--- src/frontend/apps/desk/src/pages/_app.tsx
+++ src/frontend/apps/desk/src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from 'react-i18next';
 import { AppProvider } from '@/core/';
 import { NextPageWithLayout } from '@/types/next';
 
-import './globals.css';
+import './globals.scss';
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
@@ -25,6 +25,8 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
             'La Suite administration interface: management of users and rights on the various tools (messaging, storage, etc.)',
           )}
         />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
       <AppProvider>{getLayout(<Component {...pageProps} />)}</AppProvider>
