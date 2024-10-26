9eecba011324e1d28e2eeab470e764535ebd7d80
Laurent Bossavit <laurent@MacBook-Pro-de-Laurent.local>
2024-10-26 18:53:08 +0200

    💚(ci) stricter testing
    Disable retries and save trace for failed tests.

--- src/frontend/apps/e2e/playwright.config.ts
+++ src/frontend/apps/e2e/playwright.config.ts
@@ -17,8 +17,8 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  /* Fail fast */
+  retries: 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 3 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -28,7 +28,7 @@ export default defineConfig({
     baseURL: baseURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
   },
 
   webServer: {
