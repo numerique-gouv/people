5d4efb46ad502e8718e6c2cc7529278308fd1027
Laurent Bossavit <laurent@MacBook-Pro-de-Laurent.local>
2024-11-07 18:02:02 +0100

    ✨(dimail) add basic credentials to Develop environment
    Make testing easier in a local environment by adding Test credentials.

--- src/backend/people/settings.py
+++ src/backend/people/settings.py
@@ -568,6 +568,9 @@ class Development(Base):
         }
     )
 
+    # this is a dev credentials for mail provisioning API
+    MAIL_PROVISIONING_API_CREDENTIALS = "bGFfcmVnaWU6cGFzc3dvcmQ="
+
     def __init__(self):
         """In dev, force installs needed for Swagger API."""
         # pylint: disable=invalid-name
