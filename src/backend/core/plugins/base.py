9328900b6a52cceba51337625a7268c387d51747
Quentin BEY <quentin.bey@mail.numerique.gouv.fr>
2024-12-05 18:19:11 +0100

    ✨(organizations) add siret to name conversion
    This adds the plugin system to easily manage
    Organization related customizations. This first
    plugin tries (best effort) to get a proper name
    for the Organization, using its SIRET. This
    is French specificities but another plugin can
    be defined for other cases.

---
+++ src/backend/core/plugins/base.py
@@ -0,0 +1,13 @@
+"""Base plugin class for organization plugins."""
+
+
+class BaseOrganizationPlugin:
+    """
+    Base class for organization plugins.
+
+    Plugins must implement all methods of this class even if it is only to "pass".
+    """
+
+    def run_after_create(self, organization) -> None:
+        """Method called after creating an organization."""
+        raise NotImplementedError("Plugins must implement the run_after_create method")
---
+++ src/backend/plugins/__init__.py
@@ -0,0 +1,1 @@
+"""Concrete implementation of plugins which can be used or not in the application."""
